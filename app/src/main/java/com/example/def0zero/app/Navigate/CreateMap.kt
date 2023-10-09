6525d3dfb8e75e9ea94aa2fb8a60bdfd34806cab
max <maxwell.opondo@strathmore.edu>
2023-10-09 18:26:00 +0300

    map navigation changes

--- app/src/main/java/com/example/def0zero/app/Navigate/CreateMap.kt
+++ app/src/main/java/com/example/def0zero/app/Navigate/CreateMap.kt
@@ -1,11 +1,20 @@
 package com.example.def0zero.app.Navigate
 
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.example.def0zero.R
+import com.example.def0zero.app.models.UserMap
+import com.example.def0zero.app.models.places
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,6 +24,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.def0zero.databinding.ActivityCreateMapBinding
 import com.google.android.gms.maps.model.Marker
+import com.google.android.material.snackbar.Snackbar
 
 private const val TAG = "CreateMapActivity"
 
@@ -35,17 +45,44 @@ class CreateMap : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        mapFragment.view?.let {
+            Snackbar.make(it,"Long press to add marker", Snackbar.LENGTH_INDEFINITE)
+                .setAction("OK",{})
+                .setActionTextColor(ContextCompat.getColor(this,android.R.color.white))
+                .show()
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_create_map, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // check that item is the save menu option
+        if (item.itemId == R.id.menSave){
+            Log.i(TAG,"Tapped on save!")
+            if(markers.isEmpty()){
+                Toast.makeText(this,"There must be atleast one maker on the map", Toast.LENGTH_LONG)
+                    .show()
+                return true
+
+            }
+           val places = markers.map { marker -> marker.title?.let { marker.snippet?.let { it1 ->
+               places(it,
+                   it1,marker.position.latitude, marker.position.longitude)
+           } } }
+            val userMap= intent.getStringExtra(EXTRA_MAP_TITLE)?.let { UserMap(it,places) }
+            val data = Intent()
+
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -69,16 +106,25 @@ class CreateMap : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun showAlertDialog(latLng: LatLng) {
+            val placeFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_place,null)
         val dialog =
         AlertDialog.Builder(this)
             .setTitle("Create marker")
-            .setMessage("hello")
+            .setView(placeFormView)
             .setNegativeButton("Cancel", null)
             .setPositiveButton("Ok",null)
             .show()
 
         dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
-            val marker = mMap.addMarker(MarkerOptions().position(latLng).title("my new marker"). snippet("a cool snippet"))
+            val title = placeFormView.findViewById<EditText>(R.id.edtxtTitle).text.toString()
+            val description = placeFormView.findViewById<EditText>(R.id.edtxtDesc).text.toString()
+
+            if (title.trim().isEmpty()|| description.trim().isEmpty()){
+
+                Toast.makeText(this, "place must have non--empty title and description",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val marker = mMap.addMarker(MarkerOptions().position(latLng).title(title). snippet(description))
             markers.add(marker!!)
             dialog.dismiss()
         }
--- app/src/main/java/com/example/def0zero/app/models/UserMap.kt
+++ app/src/main/java/com/example/def0zero/app/models/UserMap.kt
@@ -1,5 +1,3 @@
 package com.example.def0zero.app.models
 
-import android.icu.text.CaseMap.Title
-
-data class UserMap(val title: String, val Place: List<places>) : java.io.Serializable
+data class UserMap(val title: String, val Place: List<places?>) : java.io.Serializable
